970ed4eadbe0f73624c15e93de1379c27c7b6d77
chavez <chavezworkmail@gmail.com>
2025-08-15 20:22:47 -0500

    add rarity config

---
+++ Config/RarityColors.lua
@@ -0,0 +1,7 @@
+return {
+    Common = Color3.fromRGB(185, 182, 182),
+    Uncommon = Color3.fromRGB(138, 249, 253),
+    Rare = Color3.fromRGB(235, 178, 114),
+    Epic = Color3.fromRGB(126, 117, 255),
+    Legendary = Color3.fromRGB(218, 100, 136)
+}
